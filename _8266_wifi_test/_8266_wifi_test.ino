047cbfadd4a9490201e122f81b2035a485832238
Alex Cavnar <alc6379@gmail.com>
2017-10-28 23:59:34 -0500

    added support for power/all/control MQTT queue for turning off all machines at once

--- _8266_wifi_test/_8266_wifi_test.ino
+++ _8266_wifi_test/_8266_wifi_test.ino
@@ -1,4 +1,6 @@
 #include <dummy.h>
+
+
 #include "WiFiConstants.h"
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
@@ -19,11 +21,13 @@ const char* monitor_topic = "power/desktop/#";
 const char* log_topic = "power/desktop/log";
 const char* state_topic = "power/desktop/state";
 const char* power_topic = "power/desktop/control";
+const char* all_power_topic = "power/all/control";
 
 int counter = 0;
 
 int buttonState = DEACTIVATED;
 unsigned long logTime = 0;
+int pcState = 0;
 
 
 
@@ -37,32 +41,24 @@ void callback(char* topic, byte* payload, unsigned int length) {
   for (int i = 0; i < length; i++) {
     char receivedChar = (char)payload[i];
     Serial.print(receivedChar);
-    //    if (receivedChar == '0')
-    //      digitalWrite(ledPin, LOW);
-    //    if (receivedChar == '1')
-    //      digitalWrite(ledPin, HIGH);
   }
   Serial.println();
-  if (strcmp(power_topic, topic) == 0) {
+  if (strcmp(power_topic, topic) == 0 || strcmp(power_topic, topic) == 0) {
     Serial.println("evaluating shutdown condition");
-    char receivedChar = (char)payload[0];
-    if (receivedChar == '1') {
-      Serial.println("recieved shutdown command from MQTT");
-      pressPowerButton();
-    }
+    Serial.println("recieved shutdown command from MQTT");
+    pressPowerButton();
   }
 }
-
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266 Client", mqtt_user, mqtt_password)) {
+    if (client.connect("desktop ESP8266 Client", mqtt_user, mqtt_password)) {
       Serial.println("connected");
-      // ... and subscribe to topic
       client.subscribe(power_topic);
+      client.subscribe(all_power_topic);
       initLogQueue();
     } else {
       Serial.print("failed, rc=");
@@ -104,7 +100,7 @@ void setup()
 
   pinMode(D2, INPUT_PULLUP);
   pinMode(D3, OUTPUT);
-  pinMode(D4, INPUT_PULLUP);
+  pinMode(D5, INPUT);
   digitalWrite(D3, HIGH);
   logTime = millis();
 }
@@ -115,10 +111,8 @@ void loop()
     reconnect();
   }
   client.loop();
-
   checkButtonState();
   checkPcState();
-
 }
 
 // this won't be needed once we put this on a small 8266 board
@@ -145,24 +139,26 @@ void pressPowerButton() {
   digitalWrite(D3, LOW);
   delay(200);
   digitalWrite(D3, HIGH);
-  counter++;
   char payload[10];
   sprintf(payload, "%d", counter);
   Serial.println("incrementing log topic");
-  client.publish(log_topic, payload, 0);
+  client.publish(log_topic, "power button pressed", 0);
 }
 
 void checkPcState()
 {
   unsigned long currentTime = millis();
-  if (currentTime > (logTime + 5000)) {
-    int pcState = digitalRead(D4);
-    //    Serial.print("Checking PC power state: ");
-    //    Serial.print(pcState);
-    //    Serial.println();
-    char payload[10];
-    sprintf(payload, "%d", pcState);
-    client.publish(state_topic, payload, 1);
+  if (currentTime > (logTime + 1000)) {
+    int currentPcState = digitalRead(D5); //Need to get this to D4 when we move to small 8266 chips.
+    if (currentPcState != pcState)
+    {
+      pcState = currentPcState;
+      char payload[10];
+      sprintf(payload, "%d", pcState);
+      client.publish(log_topic, "updated PC power status", 0);
+      client.publish(state_topic, payload, 1);
+
+    }
     logTime = currentTime;
   }
 }
